2d3099fe6bff9cac55b357730223cd00196c868b
Jeremy Brady <rhspunkerkid05@yahoo.com>
2019-08-13 21:09:24 -0700

    MVP complete looking over stretch

--- src/components/ButtonComponents/NumberButtons/NumberButton.js
+++ src/components/ButtonComponents/NumberButtons/NumberButton.js
@@ -1,9 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
 
-const NumberButton = () => {
+const NumberButton = ({ number, buttonClass }) => {
   return (
     <>
+      {buttonClass === "zero" ? (
+        <button
+          onClick={() => props.concatNum(props.number)}
+          className="button zero"
+        >
+          {number}
+        </button>
+      ) : (
+        <button
+          className="button number"
+          onClick={() => props.concatNum(props.number)}
+        >
+          {number}
+        </button>
+      )}
       {/* Display a button element rendering the data being passed down from the parent container on props */}
     </>
   );
 };
+
+export default NumberButton;
--- src/components/DisplayComponents/Display.js
+++ src/components/DisplayComponents/Display.js
@@ -1,5 +1,9 @@
 import React from "react";
 
 const Display = () => {
-  return <div>{/* Display any props data here */}</div>;
+  
+  return <div className="display">
+    <div>{0}</div>
+    {/* Display any props data here */}</div>;
 };
+export default Display;
